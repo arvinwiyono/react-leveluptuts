d337f482d8fcb1724a2378e8a72355475f9809a8
Arvin Wiyono <arvinwiyono@gmail.com>
2018-03-20 23:17:56 +1100

    14. Use functional stateless component for Movie

--- src/Movie/Movie.js
+++ src/Movie/Movie.js
@@ -1,26 +1,23 @@
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import './Movie.css';
 
-export default class Movie extends Component {
-  static defaultProps = {
-    overview: 'Overview not available',
-  }
-
-  render() {
-    const { movie, overview } = this.props;
-    return (<div>
+export default function Movie({ movie, overview }) {
+  return (
+    <div>
       <h3>{movie.title}</h3>
       <p className="Movie-releaseDate">{movie.release_date}</p>
       <p className="Movie-overview">{overview}</p>
-            </div>);
-  }
+    </div>
+  );
 }
+
+Movie.defaultProps = {
+  overview: 'Overview not available',
+};
+
 // Can be written this way instead of as a static method
 Movie.propTypes = {
-  movie: PropTypes.shape({
-    title: PropTypes.string.isRequired,
-    release_date: PropTypes.string.isRequired,
-  }),
+  movie: PropTypes.shape({ title: PropTypes.string.isRequired, release_date: PropTypes.string.isRequired }).isRequired,
   overview: PropTypes.string,
 };
