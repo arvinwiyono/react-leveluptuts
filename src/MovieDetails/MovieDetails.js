4b6d71861984b7de3b7862bc30b0c3c85a2ba343
Arvin Wiyono <arvinwiyono@gmail.com>
2018-03-25 17:18:30 +1100

    18. Style movie details oage

--- src/MovieDetails/MovieDetails.js
+++ src/MovieDetails/MovieDetails.js
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import apiKeys from './../apikeys.json';
 import MovieImage from './../MovieImage';
+
+const IMAGE_ROOT_PATH = 'http://image.tmdb.org/t/p';
+const BACKDROP_PATH = `${IMAGE_ROOT_PATH}/w1280`;
 
 export default class MovieDetail extends Component {
   state = {
@@ -9,7 +13,7 @@ export default class MovieDetail extends Component {
   }
 
   async componentDidMount() {
-    try{
+    try {
       const movieDetailsUrl = `https://api.themoviedb.org/3/movie/${this.props.match.params.id}?api_key=${apiKeys.tmdb}&language=en-US`;
       const res = await fetch(movieDetailsUrl);
       const movie = await res.json();
@@ -24,13 +28,16 @@ export default class MovieDetail extends Component {
   render() {
     const { movie } = this.state;
     return (
-      <React.Fragment>
-        <h1>{movie.title}</h1>
-        <MovieImage title={movie.title} path={movie.poster_path} />
-        <MovieImage title={movie.title} path={movie.backdrop_path} backdrop />
-        <h3>{movie.release_date}</h3>
-        <p>{movie.overview}</p>
-      </React.Fragment>
+      <MovieWrapper backdrop={BACKDROP_PATH + movie.backdrop_path}>
+        <MovieInfo>
+          <MovieImage title={movie.title} path={movie.poster_path} />
+          <div>
+            <h1>{movie.title}</h1>
+            <h3>{movie.release_date}</h3>
+            <p>{movie.overview}</p>
+          </div>
+        </MovieInfo>
+      </MovieWrapper>
     );
   }
 }
@@ -42,3 +49,24 @@ MovieDetail.propTypes = {
     }),
   }).isRequired,
 };
+
+const MovieWrapper = styled.div`
+  position: relative;
+  padding-top: 50vh;
+  background: url(${props => props.backdrop}) no-repeat;
+  background-size: cover;
+`;
+
+const MovieInfo = styled.div`
+  background: white;
+  text-align: left;
+  padding: 2rem 10%;
+  display: flex;
+  > div {
+    margin-left: 40px;
+  }
+  img {
+    position: relative;
+    top: -5rem;
+  }
+`;
